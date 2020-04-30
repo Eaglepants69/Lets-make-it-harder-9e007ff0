aa205ecd7e608e016ab13b771d916587f4739c2c
Eaglepants69 <45204280+Eaglepants69@users.noreply.github.com>
2020-04-30 13:27:15 +0200

    Add files via upload

--- scale.php
+++ scale.php
@@ -1,12 +1,30 @@
 <?php
     $balans1 = 1;
-    $balans2 = 1;
+    $balans2 = 2;
+    $balans3 = array(1,2);
 
-    if($balans1 == $balans2){
-        echo "in balans";
-    }
-    else{
-        echo "niet in balans";
-    }
+    $left = $balans1 - $balans2;
     
+    if($left < 0){
+        $positive = abs($left);
+        $zoek = array_search($positive, $balans3);
+        $gelijk = $positive + $zoek;
+        if($zoek == null){
+            echo "niet in balans";      
+        }
+        elseif($gelijk == $positive){
+            echo "in balans";
+        }
+    }             
+    if($left >= 0){
+        $positive = abs($left);
+        $zoek = array_search($positive, $balans3);
+        $gelijk = $positive + $zoek;
+        if($zoek == null){
+            echo "niet in balans";      
+        }
+        elseif($gelijk == $positive){
+            echo "in balans";
+        }
+    } 
 ?>
